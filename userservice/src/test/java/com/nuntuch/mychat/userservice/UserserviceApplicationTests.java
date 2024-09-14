862e7306ea19c8f3c188eeb2f9859bfee54892ed
Nuntuch Thongyoo DT3 <nuntuch.thongyoo@gmail.com>
2024-09-14 20:41:29 +0700

    CREATE Project

---
+++ userservice/src/test/java/com/nuntuch/mychat/userservice/UserserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuntuch.mychat.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
